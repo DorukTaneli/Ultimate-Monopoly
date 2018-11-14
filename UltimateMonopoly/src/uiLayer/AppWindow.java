4bbb0db47d4550bff218d1aa03a1405612f8c7bd
barisozakar <42342283+barisozakar@users.noreply.github.com>
2018-11-14 21:41:48 +0300

    FİZX

--- UltimateMonopoly/src/uiLayer/AppWindow.java
+++ UltimateMonopoly/src/uiLayer/AppWindow.java
@@ -29,6 +29,8 @@ public class AppWindow extends JFrame implements PropertyListener{
 	private int HALFSQ;
 	private int PIECESIZE = 50;
 	private int MIDLAYER;
+	private int INNERLAYER;
+	private int OUTERLAYER;
 	private DomainController ctrl;
 	private JLabel player1Money;
 	private JLabel player2Money;
@@ -69,6 +71,8 @@ public class AppWindow extends JFrame implements PropertyListener{
 		imageIcon = new ImageIcon(newimg);  // transform it back
 		HALFSQ = (int) (BOARD_SIZE/34.5);
 		MIDLAYER = (int) (BOARD_SIZE/7.9);
+		INNERLAYER=(int) (BOARD_SIZE/3.95);
+		OUTERLAYER=(int) (BOARD_SIZE/103.75);
 		boardLabel = new JLabel(imageIcon);
 		boardLabel.setHorizontalAlignment(SwingConstants.LEFT);
 		boardLabel.setBounds(0, 0, BOARD_SIZE, BOARD_SIZE);
@@ -226,69 +230,107 @@ public class AppWindow extends JFrame implements PropertyListener{
 
 	
 	public int getPixelX(int ind) {
-//		System.out.println("Current variables: "
-//				+ "\n X_OFFSET: "+ X_OFFSET
-//				+ "\n HALFSQ: "+HALFSQ
-//				+"\n MIDLAYER: "+MIDLAYER);
-		if ((ind <= 39 && ind >= 30) || ind == 0) {
+		//		System.out.println("Current variables: "
+		//				+ "\n X_OFFSET: "+ X_OFFSET
+		//				+ "\n HALFSQ: "+HALFSQ
+		//				+"\n MIDLAYER: "+MIDLAYER);
+		//middleLayer
+		if ((ind >= 30 && ind <= 39) || ind == 0)
 			return X_OFFSET + HALFSQ*24 + MIDLAYER;
-		} else if ((ind >= 10 && ind <= 20)) {
+		else if ((ind >= 10 && ind <= 20))
 			return X_OFFSET + HALFSQ*2 + MIDLAYER;
-		}
+		//innerLayer
+		if ((ind >= 58 && ind <= 63) || ind == 40)
+			return X_OFFSET + HALFSQ*16 + INNERLAYER;
+		else if ((ind >= 46 && ind <= 52))
+			return X_OFFSET + HALFSQ*2 + INNERLAYER;
+		//middleLayer
+		if ((ind >= 106 && ind <= 119) || ind == 65)
+			return X_OFFSET + HALFSQ*32 + OUTERLAYER;
+		else if ((ind >= 78 && ind <= 92))
+			return X_OFFSET + HALFSQ*2 + OUTERLAYER;
+
 		switch (ind) {
-		case 1: case 29:
+		case 103: case 29: case 1: case 67:
 			return X_OFFSET + HALFSQ*21 + MIDLAYER;
-		case 2: case 28:
+		case 102: case 28: case 2: case 68:
 			return X_OFFSET + HALFSQ*19 + MIDLAYER;
-		case 3: case 27:
+		case 101: case 27: case 57: case 41: case 3: case 69:
 			return X_OFFSET + HALFSQ*17 + MIDLAYER;
-		case 4: case 26:
+		case 100: case 26: case 56: case 42: case 4: case 70:
 			return X_OFFSET + HALFSQ*15 + MIDLAYER;
-		case 5: case 25:
+		case 99: case 25: case 55: case 43: case 5: case 71:
 			return X_OFFSET + HALFSQ*13 + MIDLAYER;
-		case 6: case 24:
+		case 98: case 24: case 54: case 44: case 6: case 72:
 			return X_OFFSET + HALFSQ*11 + MIDLAYER;
-		case 7: case 23:
+		case 97: case 23: case 53: case 45: case 7: case 73:
 			return X_OFFSET + HALFSQ*9 + MIDLAYER;
-		case 8: case 22:
+		case 96: case 22: case 8: case 74:
 			return X_OFFSET + HALFSQ*7 + MIDLAYER;
-		case 9: case 21:
+		case 95: case 21: case 9: case 75:
 			return X_OFFSET + HALFSQ*5 + MIDLAYER;
+		case 94: case 76:
+			return X_OFFSET + HALFSQ*7 + OUTERLAYER;
+		case 93: case 77:
+			return X_OFFSET + HALFSQ*5 + OUTERLAYER;
+		case 104: case 66:
+			return X_OFFSET + HALFSQ*27 + OUTERLAYER;
+		case 105: case 65:
+			return X_OFFSET + HALFSQ*29 + OUTERLAYER;
 		default:
-			return X_OFFSET + HALFSQ*14 + MIDLAYER;
+			return HALFSQ*18;
 		}
 	}
 
 	public int getPixelY(int ind) {
-		if ((ind <= 10 && ind >= 0)) {
+		//middle Layer ifs
+		if ((ind >= 0 && ind <= 10))
 			return Y_OFFSET + HALFSQ*24 + MIDLAYER;
-		} else if ((ind >= 20 && ind <= 29)) {
+		else if ((ind >= 20 && ind <= 29))
 			return Y_OFFSET + HALFSQ*2 + MIDLAYER;
-		}
+		//inner Layer ifs
+		if ((ind >= 40  && ind <= 46))
+			return Y_OFFSET + HALFSQ*16 + INNERLAYER;
+		else if ((ind >= 52 && ind <= 58))
+			return Y_OFFSET + HALFSQ*2 + INNERLAYER;
+		//outer Layer ifs
+		if ((ind >= 64  && ind <= 78))
+			return Y_OFFSET + HALFSQ*32 + OUTERLAYER;
+		else if ((ind >= 92 && ind <= 106))
+			return Y_OFFSET + HALFSQ*2 + OUTERLAYER;
+
+		//switches
 		switch (ind) {
-		case 19: case 31:
+		case 89: case 19: case 31: case 109:
 			return Y_OFFSET + HALFSQ*5 + MIDLAYER;
-		case 18: case 32:
+		case 88: case 18: case 32: case 110:
 			return Y_OFFSET + HALFSQ*7 + MIDLAYER;
-		case 17: case 33:
+		case 87: case 17:  case 51: case 59: case 33: case 111: 
 			return Y_OFFSET + HALFSQ*9 + MIDLAYER;
-		case 16: case 34:
+		case 86: case 16: case 50: case 60: case 34: case 112:
 			return Y_OFFSET + HALFSQ*11 + MIDLAYER;
-		case 15: case 35:
+		case 85: case 15: case 49: case 61: case 35: case 113:
 			return Y_OFFSET + HALFSQ*13 + MIDLAYER;
-		case 14: case 36:
+		case 84: case 14: case 48: case 62: case 36: case 114:
 			return Y_OFFSET + HALFSQ*15 + MIDLAYER;
-		case 13: case 37:
+		case 83: case 13: case 47: case 63: case 37: case 115:
 			return Y_OFFSET + HALFSQ*17 + MIDLAYER;
-		case 12: case 38:
+		case 82: case 12: case 38: case 116:
 			return Y_OFFSET + HALFSQ*19 + MIDLAYER;
-		case 11: case 39:
+		case 81: case 11: case 39: case 117:
 			return Y_OFFSET + HALFSQ*21 + MIDLAYER;
+		case 91: case 107:
+			return Y_OFFSET + HALFSQ*5 + OUTERLAYER;
+		case 90: case 108:
+			return Y_OFFSET + HALFSQ*7 + OUTERLAYER;
+		case 80: case 118:
+			return Y_OFFSET + HALFSQ*25 + OUTERLAYER;
+		case 79: case 119:
+			return Y_OFFSET + HALFSQ*27 + OUTERLAYER;
 		default:
-			return Y_OFFSET + HALFSQ*14 + MIDLAYER;
+			return HALFSQ*18;
 		}
 	}
-
 
 
 	@Override
