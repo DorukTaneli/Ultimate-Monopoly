953f9553787182ab034669043d4f4ed24787a8ce
dtaneli16 <dtaneli16@ku.edu.tr>
2018-12-16 16:38:10 +0300

    Domain Controller JavaDoc added

--- UltimateMonopoly/src/domainLayer/DomainController.java
+++ UltimateMonopoly/src/domainLayer/DomainController.java
@@ -8,6 +8,13 @@ import domainLayer.squares.Square;
 import uiLayer.AppWindow; //THIS SHOULDNT BE HERE ************ DOING THIS TO SEE THE PIECES MOVE
 import uiLayer.PropertyListener;
 
+
+/**
+ * Domain Controller class is the controller of the Domain.
+ * Utilizes Controller Pattern. Delegates Messages from UI to relevant classes in the Domain.
+ * @author SAWCON
+ */
+
 public final class DomainController {
 	private static volatile DomainController instance = null;
 	
@@ -18,6 +25,12 @@ public final class DomainController {
 	private int turn = 0;
 	private static AppWindow app;
 
+	
+	/**
+	 * Creates Domain Controller Object.
+	 * Initializes Appwindow, Players, Cup, and PropertyListeners
+	 * @param Appwindow Object to be set 
+	 */
 	public DomainController(AppWindow aw) {
 		Player p;
 		this.app=aw;
@@ -31,7 +44,10 @@ public final class DomainController {
 				player.addPropertyListener(app);
 		}
 	}
-	
+	/**
+	 * Returns the singleton instance of Domain Controller
+	 * @return the singleton instance of Domain Controller
+	 */
 	public static DomainController getInstance() {
 		if(instance == null) {
 					instance = new DomainController(app);
@@ -54,28 +70,54 @@ public final class DomainController {
 	//	}
 	//}
 
+	/**
+	 * Gets the current PLayer
+	 * @return the current Player
+	 */
 	public Player getCurrentPlayer() {
 		return (Player) players.get(turn);
 	}
-
+	
+	/**
+	 * Gets the Players List
+	 * @return the Players List
+	 */
 	public List<Player> getPlayers() {
 		return players;
 	}
 
+	/**
+	 * Gets the Location Index of the Player with given index
+	 * @param Index of player
+	 * @return LocationIndex of Player
+	 */
 	public Player getPlayerLocationIndex(int a) {
 		return players.get(a);
 	}
 
+	/**
+	 * Gets the Board
+	 * @return the Board
+	 */
 	public Board getBoard() {
 		return board;
 	}
 
+	/**
+	 * Passes takeTurn message to the current Player
+	 * Updates UI location of player's piece
+	 */
 	public void rollPressed() {
-		Player nextPlayer = getCurrentPlayer();
-		nextPlayer.takeTurn();
+		Player currentPlayer = getCurrentPlayer();
+		currentPlayer.takeTurn();
+		//TODO make the following observer pattern
 		app.updatePieceGUILocation();
 	}
 
+	/**
+	 * If the location of the current player is a PropertySquare,
+	 * The player attempts to purchase that square.
+	 */
 	public void buyPressed() {
 		Player p = getCurrentPlayer();
 		Square sq = p.getLocation();
@@ -87,6 +129,10 @@ public final class DomainController {
 
 	}
 
+	/**
+	 * If the player has rolled,
+	 * his turn ends and it is the next player's turn.
+	 */
 	public void endTurnPressed() {
 		Player p = getCurrentPlayer();
 		if(p.haveRolled()) {
