43c029a8b3c1ed822bb28140315ba3737fccedf2
ekinozkan <ekinozkan@192.168.1.100>
2018-12-16 21:37:50 +0200

    player javadoc

--- UltimateMonopoly/src/domainLayer/Player.java
+++ UltimateMonopoly/src/domainLayer/Player.java
@@ -9,37 +9,62 @@ import domainLayer.squares.Square;
 
 import uiLayer.PropertyListener;
 
+// TODO: Auto-generated Javadoc
 /**
  * The Player class is a class which represents the players of the Monopoly game
  * Each player has a name, money, inventory of properties and cards
- * The player class implements the Publisher interface in order to publish changes in player attributes to the UI layer
- * @author SAWCON
+ * The player class implements the Publisher interface in order to publish changes in player attributes to the UI layer.
  *
+ * @author SAWCON
  */
 
 public class Player implements Publisher{
 
 	
+	/** The piece. */
 	public Piece piece;
+	
+	/** The name. */
 	private String name;
+	
+	/** The board. */
 	private Board board;
+	
+	/** The cash. */
 	private int cash;
+	
+	/** The have rolled boolean. */
 	public boolean haveRolled = false;
+	
+	/** The cup. */
 	public Cup cup;
+	
+	/** The my properties list. */
 	private ArrayList<Square> myProperties= new ArrayList<Square>();
+	
+	/** The inventory list. */
 	private ArrayList<Card> inventory=new ArrayList<Card>();
 	
+	/** The in jail boolean. */
 	public boolean inJail=false;
+	
+	/** The jail counter. */
 	private int jailCounter=0;
+	
+	/** The layer. */
 	private int layer;
+	
+	/** The index. */
 	private int index;
 	
+	/** The my listeners. */
 	private ArrayList<PropertyListener> myListeners=new ArrayList<PropertyListener>();
 
 	/**
-	 * Creates an Player object with name, board, cash, cup and piece attributes
+	 * Creates an Player object with name, board, cash, cup and piece attributes.
+	 *
 	 * @param name of the player
-	 * @param Board object of the current Monopoly game
+	 * @param board
 	 */
 	public Player(String name, Board board) {
 		this.name = name;
@@ -53,7 +78,8 @@ public class Player implements Publisher{
 	 * Gets the number of the layer that the player is currently located in
 	 * If the player stands on a square between 0 and 39, returns 1
 	 * Else if the player stands on a square between 40 and 63, returns 0
-	 * Otherwise, returns 2
+	 * Otherwise, returns 2.
+	 *
 	 * @return the number of the layer the player is located in
 	 */
 	public int whichlayer() {
@@ -72,7 +98,7 @@ public class Player implements Publisher{
 	/**
 	 * Takes turn for the current Player object, rolling the dice and moves to the corresponding square
 	 * The the player moves to the destination square one square at a time
-	 * Performs the checks for jail and rolls the dice accordingly
+	 * Performs the checks for jail and rolls the dice accordingly.
 	 */
 	public void takeTurn() {
 		if (!haveRolled) {
@@ -96,7 +122,8 @@ public class Player implements Publisher{
 	}
 	
 	/**
-	 * Gets the haveRolled attribute of this Player object, which represents whether if the current Player has already rolled or not
+	 * Gets the haveRolled attribute of this Player object, which represents whether if the current Player has already rolled or not.
+	 *
 	 * @return the haveRolled attribute
 	 */
 	public boolean haveRolled() {
@@ -104,19 +131,30 @@ public class Player implements Publisher{
 	}
 
 	/**
-	 * Sets the haveRolled attribute of this Player object to the given boolean
-	 * @param the haveRolled boolean to be set
+	 * Sets the haveRolled attribute of this Player object to the given boolean.
+	 *
+	 * @param new haveRolled boolean
 	 */
 	public void setHaveRolled(boolean haveRolled) {
 		this.haveRolled = haveRolled;
 	}
 
+	/**
+	 * Gets the name.
+	 *
+	 * @return the name
+	 */
 	public String getName() {
 		return name;
 	}
 
 	
 	
+	/**
+	 * Move one by one for f squares.
+	 *
+	 * @param f the amount of squares moved
+	 */
 	//MOVEMENT
 	public void moveOneByOneFor(int f) { 
 		
@@ -139,22 +177,39 @@ public class Player implements Publisher{
 	}
 	
 	
+	/**
+	 * Teleport to land.
+	 *
+	 * @param square to be teleported to
+	 */
 	public void teleportToLand(Square sq) {
 		piece.setLocation(sq);
 		sq.landedOn(this); 
 		publishPropertyEvent("Location", sq.getName());
 	}
 	
+	/**
+	 * Teleport without calling the landedOn function.
+	 *
+	 * @param square to be teleported to
+	 */
 	public void teleportNoLand(Square sq) {
 		piece.setLocation(sq);
 	}
 	
+	/**
+	 * Go to jail.
+	 */
 	public void goToJail() {
 		teleportNoLand(board.getSquareByIndex(10));
 		this.inJail=true;
 		System.out.println("Player "+this.name+" is in jail!");
 	}
 	
+	/**
+	 * Gets out of jail if any of the next three rolls are doubles, pays a fee at the end of 3rd turn 
+	 * to get out if none were doubles.
+	 */
 	public void tryToGetOutOfJail() { //how much cash is taken in 3 rd failed attempt
 		
 		if(cup.isDualRoll()) inJail=false;
@@ -170,11 +225,21 @@ public class Player implements Publisher{
 		
 	}
 
+	/**
+	 * Gets the location.
+	 *
+	 * @return the location
+	 */
 	public Square getLocation() {
 		return piece.getLocation();
 	}
 
 
+	/**
+	 * Adds the cash.
+	 *
+	 * @param amount of cash added
+	 */
 	//CASH
 	public void addCash(int amount) {
 		System.out.println("Player " +this.name+" gained "+amount+" money!");
@@ -182,16 +247,31 @@ public class Player implements Publisher{
 		publishPropertyEvent("Money", cash+"");
 	}
 
+	/**
+	 * Gets the cash.
+	 *
+	 * @return the cash
+	 */
 	public int getCash() {
 		return cash;
 	}
 
+	/**
+	 * Reduce cash.
+	 *
+	 * @param amount of cash reduced
+	 */
 	public void reduceCash(int amount) {
 		System.out.println("Player " +this.name+" lost "+amount+" money!");
 		cash -= amount;
 		publishPropertyEvent("Money", cash+"");
 	}
 
+	/**
+	 * Attempt purchase.
+	 *
+	 * @param property square to be purchased
+	 */
 	public void attemptPurchase (PropertySquare psq) {
 		
 			boolean success=false;
@@ -215,32 +295,68 @@ public class Player implements Publisher{
 			
 		}
 
+	/**
+	 * Adds the property.
+	 *
+	 * @param square to be added to my properties list
+	 */
 	public void addProperty(Square sq) {
 		if(!myProperties.contains(sq)) myProperties.add(sq);
 	}
 	
+	/**
+	 * Removes the property.
+	 *
+	 * @param square to be removed from my properties list
+	 */
 	public void removeProperty(Square sq) {
 		if(myProperties.contains(sq)) myProperties.remove(sq);
 		
 	}
 	
+	/**
+	 * Gets the player properties.
+	 *
+	 * @return my properties list
+	 */
 	public ArrayList<Square> getPlayerProperties(){
 		return myProperties;
 	}
 	
+	/**
+	 * Checks if is my property.
+	 *
+	 * @param square to be checked
+	 * @return true, if is in my properties list
+	 */
 	public boolean isMyProperty(Square sq) {
 		return myProperties.contains(sq);
 	}
 	
+	/**
+	 * Adds a card to the players inventory.
+	 *
+	 * @param card to be kept
+	 */
 	public void keepCard(Card card) {
 		inventory.add(card);
 	}
 	
+	/**
+	 * Uses a card's action.
+	 *
+	 * @param card to be used
+	 */
 	public void useCard(Card card) {
 		card.cardAction();
 		if(inventory.contains(card)) inventory.remove(card);
 	}
 	
+	/**
+	 * Gets the roll three cards list of player.
+	 *
+	 * @return the roll three cards list
+	 */
 	public ArrayList<Card> getRollThreeCards(){
 		ArrayList<Card> rtcards=new ArrayList<Card>();
 		for(int i=0;i<inventory.size();i++) {
@@ -251,24 +367,45 @@ public class Player implements Publisher{
 		return rtcards;
 	}
 	
+	/**
+	 * Gets the inventory list of player.
+	 *
+	 * @return the inventory list
+	 */
 	public ArrayList<Card> getInventory(){
 		return inventory;
 	}
+	
+	/**
+	 * Have rolled even boolean. True if roll is even, false otherwise.
+	 *
+	 * @return rolledEven boolean
+	 */
 	public boolean haveRolledEven(){
-		if(cup.rolledEven()==true)return true;
-		return false;
+		return cup.rolledEven();
 	}
 	
+	/**
+	 * Have rolled even boolean. True if roll is even, false otherwise.
+	 *
+	 * @return rolledOdd boolean
+	 */
 	public boolean haveRolledOdd(){
-		if(cup.rolledOdd()==true)return true;
-		return false;
+		return cup.rolledOdd();
 	}
+	
+	/* (non-Javadoc)
+	 * @see domainLayer.Publisher#addPropertyListener(uiLayer.PropertyListener)
+	 */
 	@Override
 	public void addPropertyListener(PropertyListener listener) {
 		// TODO Auto-generated method stub
 		myListeners.add(listener);
 	}
 
+	/* (non-Javadoc)
+	 * @see domainLayer.Publisher#removePropertyListener(uiLayer.PropertyListener)
+	 */
 	@Override
 	public void removePropertyListener(PropertyListener listener) {
 		// TODO Auto-generated method stub
@@ -277,6 +414,9 @@ public class Player implements Publisher{
 		}
 	}
 
+	/* (non-Javadoc)
+	 * @see domainLayer.Publisher#publishPropertyEvent(java.lang.String, java.lang.Object)
+	 */
 	@Override
 	public void publishPropertyEvent(String type, Object value) {
 		// TODO Auto-generated method stub
