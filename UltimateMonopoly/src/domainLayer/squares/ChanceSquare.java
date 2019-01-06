07ca8b62b52f3ee108a3b6e091fe593062f8ba63
dtaneli16 <dtaneli16@ku.edu.tr>
2019-01-06 17:14:04 +0300

    Randomness works on Chance and CommunityChest Cards

--- UltimateMonopoly/src/domainLayer/squares/ChanceSquare.java
+++ UltimateMonopoly/src/domainLayer/squares/ChanceSquare.java
@@ -3,28 +3,34 @@ package domainLayer.squares;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import domainLayer.Player;
 import domainLayer.cards.ChanceCard;
 import domainLayer.cards.GoToJailChanceCard;
+import domainLayer.cards.HolidayBonusChanceCard;
 import domainLayer.cards.StCharlesChanceCard;
 
 public class ChanceSquare extends Square implements Serializable{
 	
 	List<ChanceCard> cards = new ArrayList<ChanceCard>();
-	ChanceCard Jailcard = new GoToJailChanceCard("Go to Jail Card", false);
-	ChanceCard StCharlescard = new StCharlesChanceCard("Go to Jail Card", false);
+
 
 	public ChanceSquare(String name, int index) {
 		super(name, index);
 		setType("ChanceSquare");
-		// TODO Auto-generated constructor stub
+		ChanceCard JailCard = new GoToJailChanceCard();
+		ChanceCard StCharlesCard = new StCharlesChanceCard();
+		ChanceCard HolidayBonusCard = new HolidayBonusChanceCard();
+		
+		cards.add(JailCard);
+		cards.add(StCharlesCard);
+		cards.add(HolidayBonusCard);
 	}
 
 	@Override
 	public void landedOn(Player p) {
-		// TODO Auto-generated method stub
-		drawChanceCard(p);
+		ChanceCard card = drawChanceCard(p);
 		if(!card.isKeepable()) {
 			card.cardAction();
 //		}else{
@@ -32,9 +38,12 @@ public class ChanceSquare extends Square implements Serializable{
 		}			
 	}
 
-	private void drawChanceCard(Player p) {
-		// TODO Auto-generated method stub
+	private ChanceCard drawChanceCard(Player p) {
+		Random rand = new Random();
+		int val = rand.nextInt(3);
+		ChanceCard card = cards.get(val);
 		card.setDrawer(p);
+		return card;
 	}
 
 	@Override
